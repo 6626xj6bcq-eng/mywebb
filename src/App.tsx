4402ce594ef1b0d1a17a7b3a648eb0ca0947cbb4
6626xj6bcq-eng <1648361298@qq.com>
2026-02-24 11:16:27 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,28 @@ import About from './components/About';
 import Timelines from './components/Timelines';
 import GridSections from './components/GridSections';
 import Footer from './components/Footer';
+import { ScrollReveal } from './components/ScrollReveal';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen jungle-bg selection:bg-green-500/30">
       <Header />
-      <main>
-        <Hero />
-        <About />
-        <Timelines />
-        <GridSections />
+      <main className="overflow-hidden">
+        <ScrollReveal direction="none" duration={1}>
+          <Hero />
+        </ScrollReveal>
+        
+        <ScrollReveal direction="up" duration={0.8} scale>
+          <About />
+        </ScrollReveal>
+
+        <ScrollReveal direction="up" duration={0.8}>
+          <Timelines />
+        </ScrollReveal>
+
+        <ScrollReveal direction="up" duration={0.8}>
+          <GridSections />
+        </ScrollReveal>
       </main>
       <Footer />
     </div>
